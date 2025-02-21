27e82134f5f7b0827b1144c447c1eb000c687a5b
Chinaa <chinaaayaa@gmail.com>
2025-02-21 23:14:53 +0000

    new page

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { Text, View, StyleSheet } from "react-native";
 import ImageViewer from "@/components/imageViewer";
+import Button from "@/components/Button";
 
 const PlaceholderImage = require('../../assets/images/background-image.png');
 
@@ -7,6 +8,11 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}><ImageViewer imgSource={PlaceholderImage} /></View>
+
+      <View>
+        <Button label="Choose a photo"/>
+        <Button label="Use this photo"/>
+      </View>
     </View>
   );
 }
@@ -20,6 +26,10 @@ const styles = StyleSheet.create(
   },
   imageContainer: {
     flex: 1
-  }
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
   }
 );
