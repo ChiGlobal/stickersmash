8550929e4edeb5cae81dd3d65e875a1acf8db66c
Chinaa <chinaaayaa@gmail.com>
2025-02-21 21:23:02 +0000

    updated the ui of abb and statusbar

--- StickerSmash/app/(tabs)/_layout.tsx
+++ StickerSmash/app/(tabs)/_layout.tsx
@@ -1,17 +1,27 @@
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons"
 
 export default function TabsLayout() {
   return (
  <Tabs 
     screenOptions={{
-        tabBarActiveTintColor:"#0e1116"
+        tabBarActiveTintColor:"#0e1116",
+        headerStyle: {
+            backgroundColor: '#f1faee',
+          },
+          headerShadowVisible: false,
+          headerTintColor: '#0e1116',
+          tabBarStyle: {
+          backgroundColor: '#f1faee',
+          },
     }}
  >
-  <Tabs.Screen name="index" options={{headerTitle: "Sticker Smash", headerTitleAlign: "center"}} />
+  <Tabs.Screen name="index" options={{headerTitle: "Sticker Smash", headerTitleAlign: "center",
+    tabBarIcon: ({focused, color}) => <Ionicons name={focused? "home-sharp" : "home-outline"} color={color} size={25}/>}} />
   <Tabs.Screen name="about"
   options={{
-    headerTitle:"About", headerTitleAlign: "center",
-  }}/>
+    headerTitle:"About", headerTitleAlign: "center", 
+    tabBarIcon: ({focused, color}) => <Ionicons name={focused? "information-circle" : "information-circle-outline"} color={color} size={25}/>}}/>
   <Tabs.Screen name="+not-found" options={{headerShown:false}} />
  </Tabs>
  
--- StickerSmash/app/_layout.tsx
+++ StickerSmash/app/_layout.tsx
@@ -1,13 +1,15 @@
 import { Stack } from "expo-router";
-import { LogBox } from "react-native";
+import { LogBox, StatusBar } from "react-native";
 
 LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
   return (
+    <>
+    <StatusBar barStyle={"dark-content"} backgroundColor={"#f1faee"}/>
  <Stack>
-  <Stack.Screen name="(tabs)" options={{headerShown: false, headerTitleAlign: "center", headerLeft:() =><></>}} />
+  <Stack.Screen name="(tabs)" options={{headerShown: false, headerTitleAlign: "center"}} />
   <Stack.Screen name="+not-found" options={{headerShown:false}} />
- </Stack>
+ </Stack></>
   );
 }
