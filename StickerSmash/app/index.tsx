5f5ef6b3d2dd6412e8564dbe4f048b56ef01478c
Chinaa <chinaaayaa@gmail.com>
2025-02-21 18:02:56 +0000

    new page

--- StickerSmash/app/index.tsx
+++ StickerSmash/app/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Hi Expo App</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create(
+  {
+  container:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f1faee"
+  },
+  text:{
+    color:"0e1116"
+  }
+  }
+);
